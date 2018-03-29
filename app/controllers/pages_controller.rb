d70ff5ea2b48f7c54c5a9d6b50766c33bc7750f1
Carly Petracco <carly.k.p@gmail.com>
2018-03-29 22:32:26 +0100

    adding search function to the billboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,12 @@ class PagesController < ApplicationController
   def home
   end
 
+
   def billboard
+    if params[:search].nil? || params[:search].strip.empty?
+      @restaurants = Restaurant.all
+    else
+      @restaurants = Restaurant.search(params[:search])
+    end
   end
 end
