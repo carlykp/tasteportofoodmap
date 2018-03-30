e8e0a4913b570ec656ee2a6fd28859a3b194f1b9
Carly Petracco <carly.k.p@gmail.com>
2018-03-30 17:57:32 +0100

    added neightborhood model constraints

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -1,4 +1,5 @@
 class Neighborhood < ApplicationRecord
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true, inclusion: {in: ["Baixa", "Foz", "Matosinhos", "Miragaia", "Vila Nova de Gaia", "Cedofeita", "Ribeira", "Boavista", "Campanhã", "Antas", "Paranhos", "Vitoria", "Aldoar", "Afurada". "Bonfim"]}
   has_many :restaurants
+
 end
