3a628041c9ae110e88152bd023e73d8876c9b723
Carly Petracco <carly.k.p@gmail.com>
2018-03-30 17:48:38 +0100

    added neightborhood model

---
+++ app/models/neighborhood.rb
@@ -0,0 +1,4 @@
+class Neighborhood < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+  has_many :restaurants
+end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,6 +4,9 @@ class Restaurant < ApplicationRecord
   validates :food_rec, presence: true
   validates :wine_rec, presence: true
   validates :description, presence: true
+
+  belongs_to :neighborhood
+
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 end
