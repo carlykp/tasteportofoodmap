07c0bf1878c39846f80026257968c4f4da418f85
Carly Petracco <carly.k.p@gmail.com>
2018-04-11 19:06:50 +0100

    infowindow added to map

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,42 +1,63 @@
-class RestaurantsController < ApplicationController
+  class RestaurantsController < ApplicationController
 
-before_action :set_restaurant, only: [:show, :booking]
+  before_action :set_restaurant, only: [:show, :booking]
 
-  def index
+    def index
 
-    @restaurants = params[:search].nil? ? Restaurant.all.where.not(latitude: nil, longitude: nil) : Restaurant.search(params[:search]).where.not(latitude: nil, longitude: nil)
+      @restaurants = params[:search].nil? ? Restaurant.all.where.not(latitude: nil, longitude: nil) : Restaurant.search(params[:search]).where.not(latitude: nil, longitude: nil)
 
-    @markers = @restaurants.map do |restaurant|
+      @markers = @restaurants.map do |restaurant|
+        {
+          lat: restaurant.latitude,
+          lng: restaurant.longitude,
+          infoWindow: {
+             content:  '<div id="content">'+
+          '<div id="name">'+
+          restaurant.name +
+          '</div>'+
+          '<div id="description">'+
+          '<em>Description:</em> ' +
+          restaurant.description +
+          '</div>'+
+          '<div id="food">'+
+          '<em>Food recommendation:</em> ' +
+          restaurant.food_rec +
+          '</div>'+
+          '<div id="wine">'+
+          '<em>Beverage recommendation:</em> ' +
+          restaurant.wine_rec +
+          '</div>'+
+          '<div id="link">'+
+          '</div>'+
+          '</div>',
+          maxWidth: 300,
+             }
+        }
+      end
+    end
+
+    def show
+      @markers =
       {
-        lat: restaurant.latitude,
-        lng: restaurant.longitude
+        lat: @restaurant.latitude,
+        lng: @restaurant.longitude
       }
     end
+
+    def booking
+    end
+
+    private
+
+    def set_restaurant
+      @restaurant = Restaurant.find(params[:id])
+    end
+
+    def restaurant_params
+      params.require(:restaurant).permit(:name, :address, :food_rec, :wine_rec, :description, :price, :neighborhood)
+    end
+
   end
-
-  def show
-    # @markers = @restaurant.map do |rest|
-    # {
-    #   lat: rest.latitude,
-    #   lng: rest.longitude
-    # }
-    # end
-  end
-
-  def booking
-  end
-
-  private
-
-  def set_restaurant
-    @restaurant = Restaurant.find(params[:id])
-  end
-
-  def restaurant_params
-    params.require(:restaurant).permit(:name, :address, :food_rec, :wine_rec, :description, :price, :neighborhood)
-  end
-
-end
 
 
 
