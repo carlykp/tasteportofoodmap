f16bb958c511ffc7d747e14e62a7b38ca82f828e
Carly Petracco <carly.k.p@gmail.com>
2018-04-23 17:55:17 +0100

    created booking form

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,22 +3,22 @@ class BookingsController < ApplicationController
   before_action :set_restaurant, only: [:create, :index]
 
   def new
-    @booking.restaurant = @restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @booking = Booking.new
   end
 
   def create
-    @booking = Booking.new()
-    @booking.user = @user
-    @booking.restaurant = @restaurant
+    @booking = Booking.new(booking_params)
+    # we need `restaurant_id` to asssociate booking with corresponding restaurant
+    @booking.restaurant = Restaurant.find(params[:restaurant_id])
+    @booking.save
 
   end
 
   private
 
-  def set_restaurant
-    @restaurant = Restaurant.find(params[:id])
+  def booking_params
+    params.require(:booking).permit(:date, :time_1, :time_2, :guests, :email)
   end
-
-
 
 end
