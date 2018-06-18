fce73703348d8b23a3a8872f32de7c8373fb14b5
Carly Petracco <carly.k.p@gmail.com>
2018-06-18 11:04:06 +0100

    changed map view for when the search term is not found, now centers over Porto

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -7,7 +7,8 @@ if (mapElement) { // don't try to build a map if there's no div#map to inject in
   map.removeMarkers();
   map.addMarkers(markers);
   if (markers.length === 0) {
-    map.setZoom(2);
+    map.setCenter(41.149835, -8.610731);
+    map.setZoom(13);
   } else if (markers.length === 1) {
     map.setCenter(markers[0].lat, markers[0].lng);
     map.setZoom(15);
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -9,4 +9,19 @@ class Booking < ApplicationRecord
   validates :email, presence: true
   validates :telephone, length: { minimum: 9 }
 
+  validate :date_cannot_be_in_the_past, :time_must_be_in_the_future
+
+  def date_cannot_be_in_the_past
+    if date.present? && date < Date.today
+      errors.add(:date, "Cannot be in the past")
+    end
+  end
+
+  def time_must_be_in_the_future
+    if date == Date.today && time_1.present? && time_1 <= Time.now
+      errors.add(:date, "We need more time to confirm your booking")
+    end
+  end
+
+
 end
